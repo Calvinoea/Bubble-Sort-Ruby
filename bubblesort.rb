279ed034bcbdcc7fd43f1210f86a54dbcab40501
hillarioh <okerioh@gmail.com>
2020-04-23 14:16:19 +0300

    Added while

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,14 +1,23 @@
 def bubble_sort(array)
 
-  for item in array
+
+  laps = 0
+
+  #
+  while laps< array.size-1
+
+  for item in 0...array.size-1
    if array[item] > array[item+1]
      array[item], array[item+1] = array[item+1], array[item]
-    else
-     next
+   
+    end
+    
   end
-    puts array
- end
-     puts array
+  print array
+  puts ""
+  laps +=1
+end
+return array
 end
 
 bubble_sort([4,3,78,2,0,2])
