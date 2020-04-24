95183b80efabdc0d7f466b1d122f7bf2a2e6b7e3
calvinoea <calvinebunamu@gmail.com>
2020-04-24 10:37:40 +0100

    Bubble_sort_by method added

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,4 +1,4 @@
-def bubble_sort_by(array)
+def bubble_sort(array)
   laps = 0
 
   while laps < array.size - 1
@@ -12,4 +12,9 @@ def bubble_sort_by(array)
   array
 end
 
+def bubble_sort_by(array)
+  array.sort! { |first_num, second_num| first_num <=> second_num }
+end
+
 bubble_sort_by([4, 3, 78, 2, 0, 2])
+bubble_sort([4, 3, 78, 2, 0, 2])
